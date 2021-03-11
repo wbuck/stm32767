80529300f2f46934ad45de725124c58a75b5a9c8
wbuck <wbuckley15@hotmail.com>
2021-03-11 13:50:54 -0500

    Add RTIC

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -2,39 +2,18 @@
 #![no_main]
 #![allow( non_snake_case )]
 
-use cortex_m::asm;
-use cortex_m_rt::entry;
-use stm32f7xx_hal::device::{CorePeripherals, Peripherals};
 use tcp as _;
+use defmt as log;
 
-#[entry]
-fn main( ) -> ! {    
-    defmt::info!( "Hello, warren!" );
-    // Cortex-M peripherals.
-    let _core = CorePeripherals::take( ).unwrap( );
+#[rtic::app( device = stm32f7xx_hal::device, peripherals = true )]
+const APP: ( ) = {
 
-    // Device specific peripherals.
-    let device = Peripherals::take( ).unwrap( );
+    #[init]
+    fn init( _cx: init::Context ) {
+        log::warn!( "RTIC test" );
 
-    device.RCC.ahb1enr.modify( | _, w | 
-        w.gpioben( ).enabled( ) );
-
-    let gpiob = device.GPIOB;
-    gpiob.moder.modify( | _, w | w.moder14( ).output( ) );
-    gpiob.otyper.modify( | _, w | w.ot14( ).push_pull( ) );
-
-    loop { 
-        gpiob.bsrr.write( | w | w 
-            .br14( ).clear_bit( )
-            .bs14( ).set( ) );
-
-        asm::delay( 10_000_000 );
-
-        gpiob.bsrr.write( | w | w 
-            .br14( ).set_bit( ) );
-        
-        asm::delay( 10_000_000 );
+        tcp::exit( );
     }
-}
+};
 
 
